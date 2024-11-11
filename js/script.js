a68618db320ddaeb80123324845fd07e73f9f238
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-11 16:51:06 +0100

    add generate num and create li

--- js/script.js
+++ js/script.js
@@ -1,2 +1,55 @@
 "use strict"
 console.clear();
+
+//string
+const countDownString = "countdown";
+const tipsHelper = "tips";
+const rndNumber = "rndNumber";
+const rndNumberLi = "rndNumbGenerate";
+//Reference HTML
+const eleCountDown = document.getElementById(countDownString);
+const eleTips = document.getElementById(tipsHelper);
+const ulEleRndNumber = document.getElementById(rndNumber);
+
+let timeLeftCountDown = 3;
+
+for(let i = 0; i<5;i++){
+    const liEleRndNumber = document.createElement("li");
+    liEleRndNumber.classList.add(rndNumberLi);
+    liEleRndNumber.innerHTML = RandomNumGen(99,1);
+
+    ulEleRndNumber.appendChild(liEleRndNumber);
+}
+
+
+
+const countDownVar = setInterval(() => {
+    CountDownToMemorize();
+}, SetTimeSeconds(1));
+
+function GenerateRndNumber(){
+    
+}
+
+function CountDownToMemorize(){
+    eleCountDown.innerHTML = timeLeftCountDown;
+
+    timeLeftCountDown--;
+
+    if(timeLeftCountDown<0){
+        clearInterval(countDownVar);
+        ulEleRndNumber.classList.add("d-none");
+        eleTips.innerHTML = "Scrivi i numeri che hai memorizzato!!";
+    }
+}
+
+
+//Utility function
+function RandomNumGen(max,min){
+    return Math.floor(Math.random() * max+min);
+}
+
+//multiply the milliseconds in second
+function SetTimeSeconds(num){
+    return num*1000;
+}
