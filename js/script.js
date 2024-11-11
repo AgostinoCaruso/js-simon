ce18aba116f3c733c2c0a50cfd3016c4a0cef82b
AgostinoCaruso <agostinocaruso321@gmail.com>
2024-11-11 17:39:03 +0100

    input value saved in new array

--- js/script.js
+++ js/script.js
@@ -7,8 +7,11 @@ const tipsHelper = "tips";
 const rndNumber = "rndNumber";
 const rndNumberLi = "rndNumbGenerate";
 const inputUl = "inputUl";
-const inputClass = "inputNumbers";
 const btnSubmit = "btnGuessedNumbers";
+//class
+const inputClass = "inputNumbers";
+const dNoneClass = "d-none";
+
 //Reference HTML
 const eleCountDown = document.getElementById(countDownString);
 const eleTips = document.getElementById(tipsHelper);
@@ -21,60 +24,80 @@ const eleBtnSubmit = document.getElementById(btnSubmit);
 const numbToGuessArray = [];//array for generated numbers
 const numUserInsert = [];
 
-//generate li and give rnd number, then append to ul in HTML
-for(let i = 0; i<5;i++){
-    const liEleRndNumber = document.createElement("li");
-    liEleRndNumber.classList.add("rndNumberLi");
-    
-    const randomNum = RandomNumGen(99, 1);
-    liEleRndNumber.innerHTML = randomNum;
-    
-    numbToGuessArray.push(randomNum);
-    
-    ulEleRndNumber.appendChild(liEleRndNumber);
-    
-}
-console.log(numbToGuessArray);
-//generate the input numbers for guessing numbers
-for(let i = 0; i<5; i++){
-    const inputEleUserNumbers = document.createElement("input");
-    inputEleUserNumbers.classList.add(inputClass);
-    ulEleInput.appendChild(inputEleUserNumbers);
-}
-
-eleBtnSubmit.addEventListener("click", function(event){
-    event.preventDefault();
-
-});
-
+//Start program
+//set countDown timer
 let timeLeftCountDown = 3;
 const countDownVar = setInterval(() => {
     CountDownToMemorize();
 }, SetTimeSeconds(1));
 
-function GenerateRndNumber(){
-    
+//generate li and give rnd number, then append to ul in HTML
+GenerateRandomNumbers();
+console.log(numbToGuessArray);
+
+//generate the input numbers for guessing numbers
+GenerateInputNumbers();
+
+//On Click i save the value of each input in a new array
+const inputs = document.getElementsByTagName("input");
+eleBtnSubmit.addEventListener("click", function (event) {
+    event.preventDefault();
+    for (let i = 0; i < inputs.length; i++) {
+        
+        numUserInsert.push(inputs[i].value);
+    }
+    console.log(numUserInsert);
+});
+
+
+
+//function
+function GenerateInputNumbers() {
+    for (let i = 0; i < 5; i++) {
+        const inputEleUserNumbers = document.createElement("input");
+        inputEleUserNumbers.classList.add(inputClass);
+        ulEleInput.appendChild(inputEleUserNumbers);
+    }
 }
 
-function CountDownToMemorize(){
+function GenerateRandomNumbers() {
+    for (let i = 0; i < 5; i++) {
+        const liEleRndNumber = document.createElement("li");
+        liEleRndNumber.classList.add(rndNumberLi);
+
+        const randomNum = RandomNumGen(99, 1);
+        liEleRndNumber.innerHTML = randomNum;
+
+        numbToGuessArray.push(randomNum);
+
+        ulEleRndNumber.appendChild(liEleRndNumber);
+
+    }
+}
+
+function CountDownToMemorize() {
     eleCountDown.innerHTML = timeLeftCountDown;
 
     timeLeftCountDown--;
 
-    if(timeLeftCountDown<0){
+    if (timeLeftCountDown < 0) {
         clearInterval(countDownVar);
-        ulEleRndNumber.classList.add("d-none");
+        //set visibile element
+        eleCountDown.classList.add(dNoneClass);
+        ulEleRndNumber.classList.add(dNoneClass);
+        ulEleInput.classList.remove(dNoneClass);
+        eleBtnSubmit.classList.remove(dNoneClass);
+        //reset text
         eleTips.innerHTML = "Scrivi i numeri che hai memorizzato!!";
     }
 }
 
-
 //Utility function
-function RandomNumGen(max,min){
-    return Math.floor(Math.random() * max+min);
+function RandomNumGen(max, min) {
+    return Math.floor(Math.random() * max + min);
 }
 
 //multiply the milliseconds in second
-function SetTimeSeconds(num){
-    return num*1000;
+function SetTimeSeconds(num) {
+    return num * 1000;
 }
